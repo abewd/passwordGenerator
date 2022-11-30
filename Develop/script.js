e9050c2c814439c6634e263b35b5c31fbfe5443a
Abewd <116496332+abewd@users.noreply.github.com>
2022-12-01 01:24:37 +1100

    added upper case function and debugged it

--- Develop/script.js
+++ Develop/script.js
@@ -14,6 +14,52 @@ var upperCaseCheck;
 var lowerCaseCheck;
 var specialCharacterCheck;
 
+// How long do you want the PW to be? Between 8-128 characters
+
+function determineLength() {
+  passwordLength = prompt(
+    "Welcome to Abewd's sketchy PW generator. How long do you want your code to be? It can be between 8-128 chars."
+  );
+
+  if (passwordLength < 8) {
+    alert("Your password must be greater than 8 characters");
+    determineLength();
+  } else if (passwordLength > 128) {
+    alert("I said, it cant be more than 128 characters you idiot");
+    determineLength();
+  } else {
+    alert(
+      "Ok, here are some additional questions so we can generate your code:"
+    );
+  }
+  return passwordLength;
+}
+
+// Do you want the PW to have uppercase letters in it? 
+
+function determineUpperCase(){
+  upperCaseCheck = prompt({"Do you want uppercase letters in your PW? \n Yes or No"});
+  // .tolowercase = this makes all text lowercase 
+  upperCaseCheck = upperCaseCheck.toLowerCase();
+
+  // If no input into the site, display a message
+  if ("") {
+    alert("Please input Yes or No");
+    determineUpperCase();
+
+  } else if (upperCaseCheck === "yes") || (upperCaseCheck === "Yes") || (upperCaseCheck === "Y") || upperCaseCheck === "Y"{
+    upperCaseCheck = true;
+    return upperCaseCheck;
+
+  } else if (upperCaseCheck === "no") || (upperCaseCheck === "No") || (upperCaseCheck === "n") || upperCaseCheck === "N" {
+    upperCaseCheck = false;
+    return upperCaseCheck;
+
+  }
+
+
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
