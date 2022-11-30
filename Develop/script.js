919e8d173a4ea10d79573776e205a35742694532
Abewd <116496332+abewd@users.noreply.github.com>
2022-12-01 01:00:04 +1100

    added variables, and some comments

--- Develop/script.js
+++ Develop/script.js
@@ -1,201 +1,26 @@
-const generateBtn = document.querySelector("#generate");
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+var copyBtn = document.querySelector("#copy");
+var lowerCaseChar = "abcdefghijklmnopqrstuvwxyz";
+var upperCaseChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var specialCharacters = "!@#$%^&*()_+{}:<>?/";
+var numberCharacters = "0123456789";
 
-const randomLowercase = () => {
-  const lowercase = [
-    "a",
-    "b",
-    "c",
-    "d",
-    "e",
-    "f",
-    "g",
-    "h",
-    "i",
-    "j",
-    "k",
-    "l",
-    "m",
-    "n",
-    "o",
-    "p",
-    "q",
-    "r",
-    "s",
-    "t",
-    "u",
-    "v",
-    "w",
-    "x",
-    "y",
-    "z",
-  ];
-  return lowercase[Math.floor(Math.random() * lowercase.length)];
-};
+// We need to give these variables input later on in the code,
+// so for now well create them so we can assign something to them later
 
-const randomUppercase = () => {
-  const uppercase = [
-    "A",
-    "B",
-    "C",
-    "D",
-    "E",
-    "F",
-    "G",
-    "H",
-    "I",
-    "J",
-    "K",
-    "L",
-    "M",
-    "N",
-    "O",
-    "P",
-    "Q",
-    "R",
-    "S",
-    "T",
-    "U",
-    "V",
-    "W",
-    "X",
-    "Y",
-    "Z",
-  ];
-  return uppercase[Math.floor(Math.random() * uppercase.length)];
-};
+var passwordLength;
+var upperCaseCheck;
+var lowerCaseCheck;
+var specialCharacterCheck;
 
-const randomNumber = () => {
-  const num = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-  return num[Math.floor(Math.random() * num.length)];
-};
-
-const randomSpecial = () => {
-  const symbol = [
-    "!",
-    "@",
-    "#",
-    "$",
-    "^",
-    "%",
-    "&",
-    "*",
-    "(",
-    ")",
-    "{",
-    "}",
-    "[",
-    "]",
-    "=",
-    "<",
-    ">",
-    "/",
-    "?",
-    ",",
-    ".",
-  ];
-
-  return symbol[Math.floor(Math.random() * symbol.length)];
-};
-
-const getPasswordLength = () => {
-  const passwordLengthString = prompt(
-    "What is the length of the password? (should be greater than or equal to 8 and less than or equal to 128)"
-  );
-
-  return parseInt(passwordLengthString, 10);
-};
-
-const validatePasswordLength = (passwordLength) => {
-  // check if number
-  if (Number.isNaN(passwordLength)) {
-    alert("Password length should be a number");
-    return false;
-  } else {
-    if (passwordLength >= 8 && passwordLength <= 128) {
-      return true;
-    } else {
-      alert(
-        "Password length should be greater than or equal to 8 and less than or equal to 128"
-      );
-      return false;
-    }
-  }
-};
-
-const choicesToAsk = [
-  {
-    key: "isLowercase",
-    question: "Do you want to include lowercase characters?",
-    execute: randomLowercase,
-  },
-  {
-    key: "isUppercase",
-    question: "Do you want to include uppercase characters?",
-    execute: randomUppercase,
-  },
-  {
-    key: "isNumber",
-    question: "Do you want to include number characters?",
-    execute: randomNumber,
-  },
-  {
-    key: "isSpecial",
-    question: "Do you want to include special characters?",
-    execute: randomSpecial,
-  },
-];
-
-const getChoices = () => {
-  const choices = {};
-  const askUser = (choiceObject) => {
-    const confirmedChoice = confirm(choiceObject.question);
-    choices[choiceObject.key] = confirmedChoice;
-  };
-
-  choicesToAsk.forEach(askUser);
-
-  return choices;
-};
-
-const validateChoices = (choices) => {
-  const choicesArray = Object.entries(choices);
-
-  const checkIfFalse = (choice) => {
-    return !choice[1];
-  };
-
-  return !choicesArray.every(checkIfFalse);
-};
-
-const generatePassword = () => {
-  // ask the user for the password length
-  const passwordLength = getPasswordLength();
-
-  // validate the password length
-  const isValidPasswordLength = validatePasswordLength(passwordLength);
-
-  if (isValidPasswordLength) {
-    // get all 4 choices using confirm
-    const choices = getChoices();
-
-    // validate if at least one choice is true
-    const isValidChoices = validateChoices(choices);
-
-    if (isValidChoices) {
-      //
-    } else {
-      alert("Please select at least one choice");
-    }
-
-    // console.log(choices);
-  }
-};
-
-const writePassword = () => {
-  const password = generatePassword();
-  const passwordText = document.querySelector("#password");
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-};
+}
 
+// Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
